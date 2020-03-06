805ab11a46a76275a06a1296f3a25058f4f84800
gistru <58074638+gistru@users.noreply.github.com>
2020-03-06 17:04:17 +0100

    update functions

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 // Send Message
 function sendMessage($chatID,$preview,$message,$menu,$type){
   if(isset($menu)){
@@ -14,6 +17,9 @@ function sendMessage($chatID,$preview,$message,$menu,$type){
     $url=$GLOBALS[website]."/sendMessage?chat_id=$chatID&disable_web_page_preview=$preview&parse_mode=HTML&text=".urlencode($message).$button;
     file_get_contents($url);
   };
+
+
+
 // Edit Message
 function editMessage($chatID,$message_id,$preview,$newText,$menu,$type){
   if(isset($menu)){
@@ -25,19 +31,31 @@ function editMessage($chatID,$message_id,$preview,$newText,$menu,$type){
     $url=$GLOBALS[website]."/editMessageText?chat_id=$chatID&message_id=$message_id&disable_web_page_preview=$preview&parse_mode=HTML&text=".urlencode($newText).$button;
     file_get_contents($url);
   };
+
+
+
 // Send Photo
 function sendPhoto($chatID,$photo){
     $url=$GLOBALS[website]."/sendPhoto?chat_id=$chatID&photo=$photo";
     file_get_contents($url);
     };
+
+
+
 // Send Document
 function sendDocument($chatID,$document){
     $url=$GLOBALS[website]."/sendDocument?chat_id=$chatID&document=$document";
     file_get_contents($url);
     };
+
+
+
 // Send Location
 function sendLocation($chatID,$latitude,$longitude){
     $url=$GLOBALS[website]."/sendLocation?chat_id=$chatID&latitude=$latitude&longitude=$longitude";
     file_get_contents($url);
     };
+
+
+
 ?>
